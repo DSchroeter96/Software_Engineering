bf0ee63305855bbfeed193cb368f458357d5ed3a
David <dschroeter96@gmail.com>
2022-11-04 12:28:13 +0100

    implement exercise 4 design

--- lectures/Design/exercises/Q4/src/main/java/App.java
+++ lectures/Design/exercises/Q4/src/main/java/App.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -11,42 +12,54 @@ public class App {
     }
 
     public static List<Double> compute() throws FileNotFoundException {
-        File file = new File("data");
-        List<Double> normalized = new ArrayList<>();
-        Scanner scanner = new Scanner(file);
-        List<Double> numbers = new ArrayList<>();
-        while (scanner.hasNextDouble()) {
-            double number = scanner.nextDouble();
-            numbers.add(number);
-        }
-        double sum = 0;
-        for (double f : numbers) {
-            sum += f;
-        }
-        double mean = sum / numbers.size();
-        double sumSquare = 0;
-        for (double f : numbers) {
-            double diff = f - mean;
-            sumSquare += diff * diff;
-        }
-        double std = Math.sqrt(sumSquare / numbers.size());
-        for (double f : numbers) {
-            normalized.add((f - mean) / std);
-        }
+        List<Double> numbers = loadFromFile("data");
+        List<Double> normalized = normalized(numbers);
+        writeResults(normalized, "output");
         System.out.println(normalized);
-
-        try {
-            FileWriter fw = new FileWriter("output");
-            for (double n : normalized) {
-                fw.write(Double.toString(n));
-                fw.write("\n");
-            }
-            fw.close();
-        } catch (Exception e) {
-            System.out.println("Error writing output file");
-        }
-        System.out.println("Wrote output file.");
-        scanner.close();
         return normalized;
     }
+
+    private static double mean(List<Double> numbers) {
+        double sum = 0;
+        for (double f : numbers) sum += f;
+        return sum / numbers.size();
+    }
+
+    private static double variance(List<Double> numbers) {
+        double mean = mean(numbers);
+        double sum = 0;
+        for (double f : numbers) {
+            double deviation = f - mean;
+            sum += deviation * deviation;
+        }
+        return sum / numbers.size();
+    }
+
+    private static List<Double> normalized(List<Double> numbers) {
+        double standardDeviation = Math.sqrt(variance(numbers));
+        double mean = mean(numbers);
+        List<Double> normalized = new ArrayList<>();
+        for (double f : numbers) normalized.add((f - mean)/standardDeviation);
+        return normalized;
+    }
+
+    private static List<Double> loadFromFile(final String filename) throws FileNotFoundException {
+        File file = new File(filename);
+        Scanner scanner = new Scanner(file);
+        List<Double> content = new ArrayList<>();
+        while (scanner.hasNext()) content.add(scanner.nextDouble());
+        scanner.close();
+        return content;
+    }
+    
+    private static void writeResults(List<Double> normalized, String outName) {
+        try (FileWriter writer = new FileWriter(outName)) {
+            for (double f : normalized) writer.write(String.format("%f\n", f));
+        } catch (IOException e) {
+            System.out.println("Error writing output file");
+        } finally {
+            System.out.println("Wrote output file.");
+        }
+    }
+
 }

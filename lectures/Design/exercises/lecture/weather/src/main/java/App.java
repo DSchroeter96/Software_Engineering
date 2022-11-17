8eac25f24513272cf9fb183ac97f581be4a22fc7
David <dschroeter96@gmail.com>
2022-11-17 10:39:32 +0100

    implement mvp design pattern

--- lectures/Design/exercises/lecture/weather/src/main/java/App.java
+++ lectures/Design/exercises/lecture/weather/src/main/java/App.java
@@ -10,19 +10,79 @@ public class App {
     //           You can run this app on the command line with `gradlew.bat run` on Windows or `./gradlew run` on macOS and Linux.
 
     public static void main(String[] args) {
-        System.out.print("Hi! Hit Enter to check the weather forecast");
-        var scanner = new Scanner(System.in);
+        new Presenter(new RandomModel(), new ConsoleView()).run();
+    }
+}
+
+interface Model {
+    Weather getForecast();
+}
+
+interface View {
+    void run(Presenter presenter);
+    void show(String string);
+}
+
+enum Weather {
+    SUNNY("Sunny"), RAINY("Rainy"), UNKNOWN("???");
+
+    private final String s;
+
+    Weather(String s) {
+        this.s = s;
+    }
+
+    @Override
+    public String toString() {
+        return s;
+    }
+}
+
+class RandomModel implements Model {
+
+    @Override
+    public Weather getForecast() {
+        int random = ThreadLocalRandom.current().nextInt(0, 4);
+        if (random == 0) return Weather.SUNNY;
+        else if (random == 1) return Weather.RAINY;
+        else return Weather.UNKNOWN;
+    }
+}
+
+class ConsoleView implements View{
+
+    @Override
+    public void run(Presenter presenter) {
+        Scanner scanner = new Scanner(System.in);
         while (true) {
             scanner.nextLine();
-            // In a real app, this would check the weather forecast using some API, but let's simulate a prediction instead
-            int weather = ThreadLocalRandom.current().nextInt(0, 4);
-            if (weather == 0) {
-                System.out.println("Weather forecast: Sunny");
-            } else if (weather == 1) {
-                System.out.println("Weather forecast: Rainy");
-            } else {
-                System.out.println("Weather forecast: ???");
-            }
+            presenter.OnInput();
         }
     }
+
+    @Override
+    public void show(String s) {
+        System.out.println(s);
+    }
+}
+
+class Presenter {
+    private final Model model;
+    private final View view;
+
+    public Presenter(Model model, View view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void run() {
+        view.show("Hi! Hit Enter to check the weather forecast (ctrl+d to exit)");
+        view.run(this);
+    }
+
+    public void OnInput() {
+        Weather w = model.getForecast();
+        view.show("Weather forecast: " + w);
+    }
+
 }

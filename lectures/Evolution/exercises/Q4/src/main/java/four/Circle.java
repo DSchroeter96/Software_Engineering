3443f787da6402b46cf34bbd92e1b74984b83a82
David Schroeter <dschroeter96@gmail.com>
2022-11-17 21:48:34 +0100

    implement package 4 ex 4

--- lectures/Evolution/exercises/Q4/src/main/java/four/Circle.java
+++ lectures/Evolution/exercises/Q4/src/main/java/four/Circle.java
@@ -18,32 +18,14 @@ public final class Circle {
     }
 
     public double perimeter() {
-        double piApprox = 3;
-        int sign = 1;
-        for (int i = 1; i < 200_000; i++) {
-            piApprox += sign * (4.0 / ((i * 2) * (i * 2 + 1) * (i * 2 + 2)));
-            sign *= -1;
-        }
-        return 2 * piApprox * radius;
+        return 2 * Math.PI * radius;
     }
 
     public double area() {
-        int counter = 0;
-        int nb_iter = 10_000_000;
-        for (int i = 0; i < nb_iter; i++) {
-            double x = Math.random();
-            double y = Math.random();
-            double dist = Math.sqrt(x * x + y * y);
-            if (dist < 1.0)
-                counter++;
-        }
-        double piApprox = 4 * (double) counter / nb_iter;
-        return piApprox * radius * radius;
+        return Math.PI * radius * radius;
     }
 
     public double arcLength(double angleInDegrees) {
-        double piApprox = 104348.0 / 33215.0;
-        double totalLength = 2 * piApprox * radius;
-        return angleInDegrees / 360 * totalLength;
+        return angleInDegrees / 360 * perimeter();
     }
 }
